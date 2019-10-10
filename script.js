a38b1b9b692b4f81feb7995f12a1bca0a9606636
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-10-11 00:53:25 +0200

    Added caching of github repo data fetch to localStorage

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const loadReposButton = document.querySelector('button');
 
-loadReposButton.addEventListener('click', () => {
-    getAllRepos()
-        .then(data => data.map(item => item.contributors_url))
-        .then(set => console.log(set))
-});
-
 const getAllRepos = async () => {
-    console.log('getting all repos list');
+    console.log('getting all repos list...');
     const response = await fetch('https://api.github.com/orgs/angular/repos?per_page=100')
     const data = await response.json();
     return data;
 }
 
-// var req = new XMLHttpRequest();
-// req.open('GET', 'https://api.github.com/orgs/angular/repos')
+loadReposButton.addEventListener('click', () => {
+    if (localStorage.getItem('AnguRepos') === null) {
+        const repos = getAllRepos()
+            .then(data => data.map(item => item.contributors_url))
+            .then(data => localStorage.setItem('AnguRepos', data))
+            .then(console.log('localStorage object succesffuly written'))
+            .catch(err => console.log('github data fetch unsuccessful - ', err))
+
+    }
+    else {
+        console.log('Angular repositories data is already fetched and in local storage:')
+        const repos = localStorage.getItem('AnguRepos')
+        console.log(repos);
+    }
+
+});
+
+const getAllRepoContributors = async (repo) => {
+    console.log(`getting all contributors for ${repo}`)
+}
