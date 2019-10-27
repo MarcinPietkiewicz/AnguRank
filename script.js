845e17f0b85113babbc7964db450a1a8aaaa8df4
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-10-27 11:43:58 +0100

    Combined 2 request responses into one array to get all repositories, helper funcion to fetch with authorization

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const loadReposButton = document.querySelector('button');
 
-const getAllRepos = async () => {
-    console.log('fetching all angular repos list...');
-    const response = fetch("https://api.github.com/orgs/angular/repos?per_page=100", {
-        method: 'get',
-        headers: new Headers({
-            Authorization: "Basic TWFyY2luUGlldGtpZXdpY3o6YjAyZmE1NTZlYmNkNmRmMzI3MzgyODE3ZmM0MjBjNTI5ZTllMWFiYg==",
-            'Content-Type': 'application/x-www-form-urlencoded'
-        })
+const fetchWithAuthorization = async (url) => fetch(url, {
+    method: 'get',
+    headers: new Headers({
+        Authorization: "Basic TWFyY2luUGlldGtpZXdpY3o6YjAyZmE1NTZlYmNkNmRmMzI3MzgyODE3ZmM0MjBjNTI5ZTllMWFiYg==",
+        'Content-Type': 'application/x-www-form-urlencoded'
     })
-        .then(response => {
-            console.log('api requests remaining in this hour: ', response.headers.get('X-RateLimit-Remaining'));
-            return response;
-        })
-        .then(response => response.json())
-        .then(response => console.log(response))
+})
+    .then(response => { const data = response.json(); console.log(data); return data; })
+
+
+const getAllRepos = async () => {
+    const response = await Promise.all([fetchWithAuthorization("https://api.github.com/orgs/angular/repos?per_page=100&page=1"),
+    fetchWithAuthorization("https://api.github.com/orgs/angular/repos?per_page=100&page=2")])
+        .then(response => response.flat())
     return data = await response;
 }
+
+
+// const getAllRepos = async () => {
+//     console.log('fetching all angular repos list...');
+//     const response = fetch("https://api.github.com/orgs/angular/repos?per_page=100&page=2", {
+//         method: 'get',
+//         headers: new Headers({
+//             Authorization: "Basic TWFyY2luUGlldGtpZXdpY3o6YjAyZmE1NTZlYmNkNmRmMzI3MzgyODE3ZmM0MjBjNTI5ZTllMWFiYg==",
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//         })
+//     })
+//         .then(response => {
+//             console.log('api requests remaining in this hour: ', response.headers.get('X-RateLimit-Remaining'));
+//             return response;
+//         })
+//         .then(response => response.json())
+//         .then(response => { console.log(response); return response; })
+//     return data = await response;
+// }
 
 loadReposButton.addEventListener('click', () => {
     if (localStorage.getItem('AnguRepos') === null) {
